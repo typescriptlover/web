d26b8bbf45eba0472415f19ca91e7137e3a8de50
guy <devtyl3r@gmail.com>
2022-12-07 05:02:29 -0400

    fix: seo

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 export const SEO = {
-   site_name: 'personal',
-   canonical: 'https://personal.app/',
-   title: 'personal',
-   description: 'a personal website',
+   site_name: 'Vignesh Wadhwani',
+   canonical: 'https://vigy.vercel.app/',
+   title: 'Vignesh Wadhwani',
+   description: `I'm a 20-year-old Software Engineer born and living in Willemstad, Curacao, specializing in full-stack development, focused on architecting and building large-scale applications, efficient and scalable applications, and beautiful interfaces, using a wide variety of skills to complete any task.`,
    locale: 'en',
 };
