72a5c607d26bf0d9e54c566ebfceab33cdf25cf9
guy <devtyl3r@gmail.com>
2022-12-14 06:02:51 -0400

    refactor: shorter seo description

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ export const SEO: Config.SEO = {
    site_name: 'vigy.me',
    canonical: 'https://vigy.me/',
    title: 'Vignesh Wadhwani',
-   description: `I'm a 20-year-old Software Engineer born and living in Willemstad, Curacao, specializing in full-stack development, focused on architecting and building large-scale applications, efficient and scalable applications, and beautiful interfaces, using a wide variety of skills to complete any task.`,
+   description: '20 year old Software Engineer from Willemstad, Curaçao.',
    locale: 'en',
    images: [
       {
